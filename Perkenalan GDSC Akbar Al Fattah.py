087a1b3a9947a43acf8ac667b50fae0630e207d3
DeltDev <deltenc88@gmail.com>
2022-11-13 20:10:43 +0700

    initial commit

---
+++ Perkenalan GDSC Akbar Al Fattah.py
@@ -0,0 +1,4 @@
+print("Nama : Akbar Al Fattah")
+print("Jurusan : TPB STEI-K")
+print("Universitas : Institut Teknologi Bandung")
+print("Alasan ingin masuk GDSC : Saya ingin meningkatkan kemampuan bekerja sama dan kemampuan programming")
