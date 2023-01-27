68a9bc456cd1e856fe935ab4a936068a903788eb
DeltDev <deltenc88@gmail.com>
2023-01-27 23:30:47 +0700

    Hands on OOP submission by Akbar Al Fattah

--- Perkenalan GDSC Akbar Al Fattah.py
+++ Perkenalan GDSC Akbar Al Fattah.py
@@ -1,4 +1,4 @@
 print("Nama : Akbar Al Fattah")
 print("Jurusan : TPB STEI-K")
 print("Universitas : Institut Teknologi Bandung")
-print("Alasan ingin masuk GDSC : Saya ingin meningkatkan kemampuan bekerja sama dan kemampuan programming")
+print("Alasan ingin masuk GDSC : Saya ingin meningkatkan kemampuan bekerja sama dan kemampuan programming serta menambah pengalaman dalam dunia programming")
